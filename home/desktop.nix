5f988a897b5cec5dc01764125bac7c92e4138051
Adrien5902 <adrien.m5902@gmail.com>
2026-09-12 17:09:09 +0200

    move hyprland config to separate file

--- home/desktop.nix
+++ home/desktop.nix
@@ -21,11 +21,8 @@
     ./spicetify.nix
     ./waybar
     ./obsididan.nix
+    ./hyprland.nix
   ];
 
-  home.file.".config/hypr/hyprland.lua".source = ./hyprland/hyprland.lua;
-  home.file.".config/hypr/lua".source = ./hyprland/lua;
-  home.file.".config/hypr/hypridle.conf".source = ./hyprland/hypridle.conf;
-  home.file.".config/hypr/xdph.conf".source = ./hyprland/xdph.conf;
   home.file.".local/bin".source = ./scripts;
 }
---
+++ home/hyprland.nix
@@ -0,0 +1,6 @@
+{
+  home.file.".config/hypr/hyprland.lua".source = ./hyprland/hyprland.lua;
+  home.file.".config/hypr/lua".source = ./hyprland/lua;
+  home.file.".config/hypr/hypridle.conf".source = ./hyprland/hypridle.conf;
+  home.file.".config/hypr/xdph.conf".source = ./hyprland/xdph.conf;
+}
